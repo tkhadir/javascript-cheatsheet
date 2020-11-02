f03ff6cdda8195f4f3f9fe5f62c3b55324086d48
KHADIR Tayeb <45130488+tkhadir@users.noreply.github.com>
2020-11-02 14:51:59 +0100

    Update samples.js

--- samples.js
+++ samples.js
@@ -1,5 +1,9 @@
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('test.db'); //:memory:
+
+let create_json_table_demo = 'CREATE TABLE t1 (jdoc JSON)'
+let insert_json_demo = 'INSERT INTO t1 VALUES(\'{"key1": "value1", "key2": "value2"}\')'
+let select_json_demo = 'SELECT * from t1'
  
 db.serialize(function() {
   db.run("CREATE TABLE lorem (info TEXT)");
@@ -13,6 +17,12 @@ db.serialize(function() {
   db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
       console.log(row.id + ": " + row.info)
   });
+ 
+  db.run(create_json_table_demo)
+  db.run(insert_json_demo)
+  db.each(select_json_demo, (err, row) => {
+    console.log(row.jdoc)
+  })
 });
  
 db.close()
